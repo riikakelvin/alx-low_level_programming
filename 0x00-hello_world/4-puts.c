0ea42af0352aa19514d853178c3a26144b5b1500
riikakelvin <riikakelvin2@gmail.com>
2023-03-16 11:36:37 -0700

    commit for hello puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - making use of the put function to create a prog that prints out a given line of writing
+ * return:(0) 
+ */
+int main(void)
+{
+puts("\Programming is like building a multilingual puzzle);
+return (0);
+}
