0048c2e707788638bb6a231de09c88a47c91b610
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 15:19:57 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,28 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /* main - Start of the program*/
 /*Description: program that prints if numbers are positive negative values*/
-/* Return: (0) is always a success*/
-
+/* Return: is always a success*/
 
 int main(void)
-{	int n;
+{
+	int n, lastd;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{printf("%d is positive\n", n);
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{printf("last digit of %d is %d and is greater than 5\n", n);
 	}
-	else if (n == 0)
-	{printf("%d is zero\n", n);
+	else if (lastd == 0)
+	{printf("last digit of %d is %d and is 0\n", n);
 	}
-	else
-	{"$d is negative\n", n);
+	else if (lastd < 6 && lastd != 0)
+	{"last digit %d is %d and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
+
