b2b0caf82a77666f2c6009f3e51c2ebfcdbe7923
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 14:45:16 -0700

    committed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,11 @@
 
 
 int main(void)
-{	int n;
-	lastd = n % 10;
+{	int n; lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
 
 	if (lastd > 5)
 	{printf("last digit of %d is %d and is greater than 5\n", n);
