77f382cbb92abb400d774c25fc2bcd89185bbea9
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 14:38:03 -0700

    commit for task 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,17 +11,17 @@
 
 int main(void)
 {	int n;
-	srand(time(0));
+	srand(time(0))
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{printf("%d is positive\n", n);
+	if (lastd > 5)
+	{printf("last digit of %d is %d and is greater than 5\n", n);
 	}
-	else if (n == 0)
-	{printf("%d is zero\n", n);
+	else if (lastd == 0)
+	{printf("last digit of %d is %d and is 0\n", n);
 	}
-	else
-	{"$d is negative\n", n);
+	else if (lastd < 6 && lastd != 0)
+	{"last digit %d is %d and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
