d3f93fd31dd8bab5922c7d4587c116d0415b57c8
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 16:02:25 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,16 +5,16 @@
 /**
 * main - Start of the program
 * Description: program that prints if numbers are positive negative values
-* Return: is always a success
+* Return: 0 is always a success
 */
 
 int main(void)
-{
+{	
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{printf("%d is positive", n);
 	}
@@ -24,5 +24,6 @@ int main(void)
 	else
 	{printf("%d is negative", n);
 	}
+	printf("\n");
 	return (0);
 }
