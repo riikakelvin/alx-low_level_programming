0bc71ac5892ca8a3a71cae33f580b0bc53390ed0
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 14:11:59 -0700

    comit for task 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,28 @@
 #include <stdlib.h>
+
 #include <time.h>
-/* Main - Start point for the program */
 
-/* Return: (0) indicates success */
+#include <stdio.h>
+
+
+/** main - program that prints if numbers are positive negative values
+ * Return: (0) is always a success
+ *
+ */
+
 int main(void)
-{
-	int n;
-
+{	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{printf("%d is positive\n", n);
 	}
 	else if (n == 0)
-	{ptintf("%d is positive\n", n);
+	{printf("%d is zero\n", n);
 	}
 	else
-	{printf(printf("%d is negative\n", n);
+	{"$d is negative\n", n);
+	}
 	return (0);
 }
