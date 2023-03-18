9cba6bd6947899e717bca307f57267c3212dfa4a
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 17:17:01 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,13 @@
  * main - main function 
  *
  * return is 0 (Success)
- * 
  */
 int main(void)
 {	
 	char i;
 
 	for (i = 'a'; i <= 'z' ; i++)
-		putchar(i);
+	putchar(i);
 	putchar('\n');
 	return (0);
 }
