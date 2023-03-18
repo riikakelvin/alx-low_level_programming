6b2235f5d0f1898e7edf53e7758616d52bf41b04
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 17:53:54 -0700

    commit for 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  starting point for the program
+ *
+ * return: 0 (Success)
+ *
+ */
+int main(void)
+{	int i;
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
