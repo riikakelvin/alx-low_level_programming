fa976aba601eaae4066c82236b4b857a8a581e7c
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 17:15:23 -0700

    commit for 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function 
+ *
+ * return is 0 (Success)
+ * 
+ */
+int main(void)
+{	
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
