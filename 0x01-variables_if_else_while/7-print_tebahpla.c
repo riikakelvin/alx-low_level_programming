2df487896418a184cd9ef01589cd963ed8a403d6
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 18:16:45 -0700

    commit for 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -starting point for the program
+ *
+ * return: 0 (Success)
+ */
+int main(void)
+{	char i;
+	for (i = 'z' ; i <= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0); 
+}
+
+
