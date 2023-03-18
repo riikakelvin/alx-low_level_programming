c3c44c4850597ef766d2a0241978e96c291adc89
riikakelvin <riikakelvin2@gmail.com>
2023-03-17 18:02:40 -0700

    commit for task 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point for the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
