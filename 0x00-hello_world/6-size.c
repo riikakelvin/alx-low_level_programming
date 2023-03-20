e7521ea3d6b734114cc0058db3bca57883fc413b
riikakelvin <riikakelvin2@gmail.com>
2023-03-20 10:28:47 -0700

    committed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,7 @@
  * Return: 0 (Successful test)
  */
 int main(void)
-{	char;
-	int;
-	long int;
-	long long int;
-	float;
+{	
 	printf("Size of a char: %i byte(s)\n", (sizeof(char));
 	printf("Size of an int: %i byte(s)\n", (sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", (sizeof(long int));
