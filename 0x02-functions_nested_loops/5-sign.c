5eb436e20231732df41c985b50a0de432aa24369
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 14:16:02 -0700

    commit for task 5 for a program that  prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - to print the sign of a number
+ * @n: the number to be checked by the program
+ * Returns 1 and prints + if n is greater than zero 
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{_putchar(43);
+return (1);
+}
+else if (n < 0)
+{_putchar(45);
+return (-1);
+}
+else
+{_putchar(48);	
+return(0);
+}
+}
