c2f0ae870a885be6f17d226bcebb8fb03c55bbf8
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 12:39:49 -0700

    commit for 1 alphabet listing task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - to make the alphabet required
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+_putchar(alphabet);
+_putchar('\n');
+}
