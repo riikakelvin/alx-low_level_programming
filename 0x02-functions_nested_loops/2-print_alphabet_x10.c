6af882ae6f1c4518efafe8e3ec774da6c5eb1c14
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 12:52:28 -0700

    commit for task 2-print_alphabet_x10 task printing out alphabet ten folds

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints out the alphabet ten folds
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int i;
+	i = 0;
+	while (i > 10)
+	{ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	_putchar(alphabet);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
