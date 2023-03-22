8706f8227b95a40dd9728f69487f73d8db26edf0
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 12:18:15 -0700

    commit for task 1 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - entry point for the program printing _putchar
+ * main - to print _putchar
  *
- * return: 0 always
+ * Return: 0 when successful
  */
 int main(void)
-{printf("_putchar"\n);
-	return (0);
+{_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
