5ddc9479bdc68b7555ac410ad25aeda8bcbbb87f
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 15:23:37 -0700

    commit for time display task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,12 +15,12 @@ for (l = 0; l <= 5; l++)
 {
 for (v = 0; v <= 9; v++)
 {
-        _putchar(k + '0');
-        _putchar(e + '0');
-        _putchar(58);
-        _putchar(l + '0');
-        _putchar(v + '0');
-        _putchar('\n');
+_putchar(k + '0');
+_putchar(e + '0');
+_putchar(58);
+_putchar(l + '0');
+_putchar(v + '0');
+_putchar('\n');
 }
 }
 }
