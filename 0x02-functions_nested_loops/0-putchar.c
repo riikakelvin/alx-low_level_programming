8bd9e189d2cb07e96b950717acd4d5a45a620d61
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 10:16:21 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point for the program printing _putchar
+ *
+ * return: 0 always
+ */
+int main(void)
+{printf("_putchar"\n);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readmefile for 0x02-functions_nested_loops
