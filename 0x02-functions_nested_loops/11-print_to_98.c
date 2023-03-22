696b1104d14005fa3e0a5e936c0978cbd72ec99c
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 16:58:06 -0700

    commite

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,22 @@ void print_to_98(int n)
 	break;
 	}
 	else
-	{printf("%d,", n);
+	{
+	printf("%d", n);
+	}
+	}
+	}
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d", n);
 	}
 	}
 	}
