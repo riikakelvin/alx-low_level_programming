081a6f3cf8fa3b78bbabae941bcccdeb2166042c
riikakelvin <riikakelvin2@gmail.com>
2023-03-22 16:03:57 -0700

    commit for task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,16 +5,17 @@
  */
 void times_table(void)
 {
-	int k, e, v, i, n ;
+	int k, e, v, i, n;
+
 	for (k = 0; k <= 9; k++)
 	{
-	for (e = 0; e <= 9; e ++)
+	for (e = 0; e <= 9; e++)
 	{
 	v = k * e;
 	if (v > 9)
 	{
 	i = v % 10;
-	n = ( v - i)/ 10;
+	n = (v - i) / 10;
 	_putchar(44);
 	_putchar(32);
 	_putchar(n + '0');
