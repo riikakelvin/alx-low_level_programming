5510352b275019ece74d7341ad6374ca150a3fa2
riikakelvin <riikakelvin2@gmail.com>
2023-03-23 12:21:42 -0700

    commmit for task 4 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints number 0 to 9, except 4 and 2
+ * Return: numbers between 0 and 9, excluding 2 and 4
+ */
+void print_most_numbers(void)
+{	int k = 0;
+
+	for (; k <=9; k++)
+	{
+	if (k == 4 || k == 2)
+	continue;
+	}
+	else
+	{
+	_putchar(k + '0');
+	}
+	_putchar("\n");
+}
