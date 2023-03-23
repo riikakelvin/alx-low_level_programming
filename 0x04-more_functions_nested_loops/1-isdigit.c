af31f0f7f0dd8c47824acfc25c0896c04b709100
riikakelvin <riikakelvin2@gmail.com>
2023-03-23 08:00:46 -0700

    commit for  function that checks for a digit (0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit between 0 and 9
+ * @c: is the numbers under examination
+ * Return: 1 for numbers less than  or equal to 9, otherwise return 0
+ */
+int _isdigit(int c)
+{if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
