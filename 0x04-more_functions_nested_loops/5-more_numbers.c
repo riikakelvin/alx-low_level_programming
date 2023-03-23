7dd17efe60342dccb3c2bcad93f5d3811752a02d
riikakelvin <riikakelvin2@gmail.com>
2023-03-23 12:45:58 -0700

    commit for task 5 of 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers 0 to 14, 10 folds
+ * @v: the numbers under consideration
+ * @k: the number of times of repeat printing
+ * Return: the numbers between 0 and 14 ten times
+ */
+void more_numbers(void)
+{	int v;
+	int k;
+
+	for(v = 0; v <= 14; v++)
+	{
+	for(k = 0; k < 10; k++)
+	{
+	if(k > 9)
+	{
+	_putchar((y/ 10) + '0');
+	{
+	_putchar((y% 10) + '0');
+	{
+	_putchar('\n');
+	}
+	}
+	}
+	}
+	}
+}
