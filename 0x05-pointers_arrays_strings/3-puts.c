fe5cd88970ebf653def9245ba7b14261000f4108
riikakelvin <riikakelvin2@gmail.com>
2023-03-28 11:54:49 -0700

    commit for a function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * _puts - prints the string
+ * @str: the string to be printed
+ */
+void _puts(char *str)
+{	char *str;
+	while (*str != '/0')
+	{
+		_putchar(str*++);
+	}
+	_putchar('\n');
+}
