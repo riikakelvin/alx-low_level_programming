273b9fe827241fb533e16f41cab39e531dead654
riikakelvin <riikakelvin2@gmail.com>
2023-03-29 13:03:54 -0700

    commit for a function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp -  compares two strings
+ * @s1: string value two
+ * @s2: string value one
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{	int k;
+
+	for (k = 0; s1[k] != '\0' || s2[k] != '\0'; k++)
+	{
+	if (s1[k] < s2[k])
+	{
+		return (s1[k] - s2[k]);
+	}
+	else if (s1[k] > s2[k])
+	{
+		return (s1[k] - s2[k]);
+	}
+	}
+	return (0);
+}
