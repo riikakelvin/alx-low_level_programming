fc9c3a204b3b3a52dcfc624691dc331917b68589
riikakelvin <riikakelvin2@gmail.com>
2023-03-30 13:49:53 -0700

    commit for a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,32 @@
 #include "main.h"
 /**
  * cap_string - capitalizes all words of a string
- *
- *
+ * @str: string awaiting capitalization
+ * Return: pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int k = 0;
+
+	while (str[k])
+	{
+		while (!(str[k] >= 'a' && str[k] <= 'z'))
+			k++;
+		if (str[k] - 1 == ' ' ||
+		str[k] - 1 == '\t' ||
+		str[k] - 1 == '\n' ||
+		str[k] - 1 == ',' ||
+		str[k] - 1 == '.' ||
+		str[k] - 1 == '!' ||
+		str[k] - 1 == '?' ||
+		str[k] - 1 == '"' ||
+		str[k] - 1 == '(' ||
+		str[k] - 1 == ')' ||
+		str[k] - 1 == '{' ||
+		str[k] - 1 == '}' ||
+		k == 0)
+		str[k] -= 32;
+		k++;
+	}
+	return (str);
+}
