060abe1a17760ccc675c071f7ced7fafb97e703c
riikakelvin <riikakelvin2@gmail.com>
2023-03-30 16:44:01 -0700

    commit for task 10, magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - main function
+ * Return: 0 always
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
+a[2] = 1024;
+p = &n;
   /*
    * write your line of code here...
    * Remember:
