d20b7a8aa0b426feaf061e3e09b23cee948fb67e
riikakelvin <riikakelvin2@gmail.com>
2023-03-30 15:48:31 -0700

    commit for task 10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -entry point, magic
+ *
+ * Return: 0
+ */
+int main(void)
+{	int k;
+	int e[s];
+	int *v;
+
+	e[2] = 1024;
+	v = &k;
+
+	*(v + s) = 98;
+	{
+		printf("e[2] = %d\n", e[2]);
+	}
+	return (0);
+}
