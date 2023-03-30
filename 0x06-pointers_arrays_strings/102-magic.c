48943725eec7064b78233da9e649b9e38dbf52e2
riikakelvin <riikakelvin2@gmail.com>
2023-03-30 16:37:46 -0700

    commit for the magic task 109

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - main function
+ * @p: pointer
  * Return: 0 always
  */
 int main(void)
@@ -19,7 +20,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-	*(p + 5) = 98;
+*(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
