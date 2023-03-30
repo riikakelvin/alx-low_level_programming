b4bf627f3407681ba166d216782318faff4617f9
riikakelvin <riikakelvin2@gmail.com>
2023-03-30 16:40:53 -0700

    addedmissing statement to  magic task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-/**
- * main - main function
- * @p: pointer
- * Return: 0 always
- */
+
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
+  a[2] = 1024;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
