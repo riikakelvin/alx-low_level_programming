fd4051ea8f8b2e8f5d8a3bad4f49ae03751bf303
riikakelvin <riikakelvin2@gmail.com>
2023-03-30 15:11:09 -0700

    commit for a function that prints an integer.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer under consideration
+ * Return: 0
+ */
+void print_number(int n)
+{	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
+}
