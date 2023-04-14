3eb5ec14d1a1626bb97560f4a763c816e74022bc
riikakelvin <riikakelvin2@gmail.com>
2023-04-14 03:45:09 -0700

    commit for a function that links two strings together

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * str_concat - concatenates two strings
  * @s1: first string under consideration
@@ -12,41 +12,38 @@ char *str_concat(char *s1, char *s2)
 {	char *str;
 	unsigned int l1, l2, size_occupied, k, e;
 
-	if (s1 == NULL)
+	l1 = l2 = 0;
+
+	if (s1 != NULL)
 	{
-		s1 = " ";
+		k = 0;
+		while (s1[k++] != '\0')
+			l1++;
 	}
+
+	l1 = 0;
+
 	if (s2 == NULL)
 	{
-		s2 = " ";
-	}
-	l1 = 0;
-	while (s1[l1] != '\0')
-	{
-		l1++;
-	}
-	while (s2[l2] != '\0')
-	{
+		k = 0;
+		while (s2[k++] != '\0')
 		l2++;
 	}
+
 	size_occupied = l1 + l2;
-		str = malloc((sizeof(char) * (size_occupied) + 1));
+		str = (char *)malloc(sizeof(char) * (size_occupied + 1));
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	k = 0;
-	while (k < l1)
+	for (k = 0; k < l1; k++)
 	{
 		str[k] = s1[k];
-		k++;
 	}
-	e = 0;
-	while (k <= size_occupied)
+	for (e = 0; e < l2; e++)
 	{
-		str[k] = s2[e];
-		k++;
-		e++;
+		str[e] = s1[e];
 	}
-	return (*str);
+	str[size_occupied] != '\0';
+	return (str);
 }
