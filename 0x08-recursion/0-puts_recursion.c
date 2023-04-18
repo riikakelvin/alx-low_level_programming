6618326a1954214a0d866f8ae5b454bbe33635c8
riikakelvin <riikakelvin2@gmail.com>
2023-04-18 06:43:10 -0700

    commit for  a function that prints a string, followed by a new line recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	s = "kelvin\0";
-
 	if (*s == '\0')
 	{
 		return;
 		_putchar('\n');
 	}
+	else
 	{
 		_putchar(*s);
 		s++;
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
