3c7755894abb60b1a9e0154f4917f36c28379f49
riikakelvin <riikakelvin2@gmail.com>
2023-04-18 07:10:35 -0700

    commit for  afunciton that writes a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string parameter
+ * Return 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	putchar(*s);
+}
