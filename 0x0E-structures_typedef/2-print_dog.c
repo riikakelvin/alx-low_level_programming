b95a315e214f68dbde62d5394624b0cfd285d183
riikakelvin <riikakelvin2@gmail.com>
2023-04-18 17:16:34 -0700

    commit for task 3, to Write a function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)\n");
+		printf("Name:(nil)\n");
 	}
 	else
 	{
@@ -21,18 +21,18 @@ void print_dog(struct dog *d)
 	}
 	if (d->age < 0)
 	{
-		printf("age : (nil)\n");
+		printf("Age: (nil)\n");
 	}
 	else
 	{
-		printf("age :: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 	}
 	if (d->owner == NULL)
 	{
-		printf("owner :: (nil)\n");
+		printf("Owner:(nil)\n");
 	}
 	else
 	{
-		printf("owner :: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 }
