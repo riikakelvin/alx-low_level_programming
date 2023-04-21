c0bd01a32c4423ab34ec9ff5e8e443bbeb8a1756
riikakelvin <riikakelvin2@gmail.com>
2023-04-20 19:10:39 -0700

    commited for task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -32,7 +32,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%s%s", separator, str);
+		printf("%s%s", separator, str);
 		}
 		va_end(strings);
 	}
