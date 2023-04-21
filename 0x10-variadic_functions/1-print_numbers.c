a97c7a5a295f76271700fc4726b734f9b162460a
riikakelvin <riikakelvin2@gmail.com>
2023-04-20 17:48:45 -0700

    commit for taks 1 To be is to be the value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to function
+ * Return:
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator)
+		{
+			printf("%d\n", va_arg(list, int));
+		}
+		else if (separator && i == 0)
+		{
+			printf("%d\n", va_arg(list, int));
+		}
+		else
+		{
+			printf("%s\n%d\n", separator, va_arg(list, int));
+		}
+		va_end(list);
+	}
+	printf("\n");
+
+}
