d1001836e2d7c9f349e12669a2d47d47d01b1f2f
riikakelvin <riikakelvin2@gmail.com>
2023-04-28 03:08:38 -0700

    commit for task2, for a function that adds a new node at the beginning of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * listint_t *add_nodeint - adds a new node at the beginning of list
+ * @head: start of the list
+ * @n: input data addition to code
+ * Return: pointer to the new node, otherwise NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
