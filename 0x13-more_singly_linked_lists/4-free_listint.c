311ecf4c69776ca624384b1a658ade70ba868e52
riikakelvin <riikakelvin2@gmail.com>
2023-04-28 03:30:08 -0700

    commit for task 5, a function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list
+ * @head: list to be freed (listint_t)
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
