79e721dd07f329ff0c5bf39a743fe6b924cb852f
riikakelvin <riikakelvin2@gmail.com>
2023-04-27 19:34:47 -0700

    commit for taks 3 of singly linked lsist

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
@@ -11,22 +9,21 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-
 	unsigned int len = 0;
+	size_t n;
 
-	while (str[len])
-	{
-		len++;
-		new_node = malloc(sizeof(list_t));
-	}
-	if (!new_node)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 	{
 		return (NULL);
+	}
+	new_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+	{
 		new_node->str = strdup(str);
 		new_node->len = len;
 		new_node->next = (*head);
-		(*head) = new_node;
 	}
 return (*head);
 }
-
